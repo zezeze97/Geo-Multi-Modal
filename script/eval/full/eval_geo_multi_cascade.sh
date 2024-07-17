428e327b44bd3781cc2e1b2e79459f3a2d18041d
uglyboy <uglyboy@uglyboy.cn>
2024-07-17 15:02:56 +0000

    123

--- script/eval/full/eval_geo_multi_cascade.sh
+++ script/eval/full/eval_geo_multi_cascade.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 question_file=$1
+qa_mode=$2
 
 # Assign the command line arguments to variables
 N=8
-base_answer_path="./outputs/bunny-yi1.5-9B-Chat-FormalGeoCoT-VisionPretrained-sft1e-v10/${question_file}"
+base_answer_path="./outputs/bunny-yi1.5-9B-Chat-FormalGeoCoT-VisionPretrained-sft1e-v9/${question_file}_{qa_mode}"
 gpus=(0, 1, 2, 3, 4, 5, 6, 7)  # Define the GPU IDs array
 
 
@@ -19,8 +20,8 @@ do
     # --vision_encoder_path checkpoints/checkpoints-yi1.5/bunny-yi1.5-9B-rerun \
     # 
     CUDA_VISIBLE_DEVICES="${gpus[chunk_id]}" python bunny/eval/model_vqa.py --model-type yi1.5 \
-                                                                     --model-path checkpoints/checkpoints-yi1.5/bunny-yi1.5-9B-Chat-FormalGeoCoT-VisionPretrained-sft1e-v10 \
-                                                                     --vision_encoder_path checkpoints/checkpoints-yi1.5/bunny-yi1.5-9B-Chat-FormalGeoCoT-VisionPretrained-sft1e-v10 \
+                                                                     --model-path checkpoints/checkpoints-yi1.5/bunny-yi1.5-9B-Chat-FormalGeoCoT-VisionPretrained-sft1e-v9 \
+                                                                     --vision_encoder_path checkpoints/checkpoints-yi1.5/bunny-yi1.5-9B-Chat-FormalGeoCoT-VisionPretrained-sft1e-v9 \
                                                                      --question-file data/formalgeo7k/formalgeo7k_v2/custom_json/qa_resoning/geoqa_test/${question_file}.jsonl \
                                                                      --answers-file "$answer_path" \
                                                                      --num-chunks "$N" \
@@ -28,7 +29,13 @@ do
                                                                      --image-folder data/formalgeo7k/formalgeo7k_v2 \
                                                                      --temperature 0 \
                                                                      --crop \
-                                                                     --conv-mode yi-chat &
+                                                                     --conv-mode yi-chat \
+                                                                     --cascade-mode \
+                                                                     --recong-model-type qwen2 \
+                                                                     --recong-model-path checkpoints/checkpoints-qwen2/bunny-lora-qwen2-qa-FormalGeoV2Aug10Times_calibrate_structure_only-sft4/merged \
+                                                                     --recong-model-vision_encoder_path checkpoints/checkpoints-qwen2/bunny-lora-qwen2-qa-FormalGeoV2Aug10Times_calibrate_structure_only-sft4/merged \
+                                                                     --recong-conv-mode qwen-chat \
+                                                                     --process-meta-qs-mode "$qa_mode" & # Q+PredCDL2Ans/Q+PredCDL2CalibrateCDLandAns
 
     # Uncomment below if you need a slight delay between starting each process
     # sleep 0.1
